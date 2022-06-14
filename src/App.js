a04b41e439e8f21d578f082433abc7847a1dc808
Luis Sanchez <97128550+stuffy-doll@users.noreply.github.com>
2022-06-14 12:50:13 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import Home from './components/Home';
-import Stocks from './components/Stocks';
-import Movies from './components/Movies';
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
+const { Route, Switch } = require("react-router-dom");
 
 function App() {
   return (
-    <div className='main'>
+    <div className="main">
       <h1>App Component</h1>
-      <Home />
-      <Stocks />
-      <Movies />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route>
+          <h1>Page Not Found</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+const { BrowserRouter } = require("react-router-dom");
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
