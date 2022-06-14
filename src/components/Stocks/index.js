2d7a3f98aef8d85f0f58dbcb36563a350f255b02
Luis Sanchez <97128550+stuffy-doll@users.noreply.github.com>
2022-06-14 13:35:03 -0700

    redirect and history

--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,7 +1,20 @@
+const { Redirect, useHistory } = require("react-router-dom");
+
 function Stocks() {
+  let loggedIn = true;
+  const history = useHistory();
+  const handleClick = () => {
+    window.alert("Sending info to the DB!");
+    history.push("/");
+  };
+  console.log(history);
+  if (loggedIn === false) {
+    return <Redirect to="/not-logged-in"></Redirect>;
+  }
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
